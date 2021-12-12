d61b98280224292fde6b20aa95e2038c3a135b2d
jeffvswanson <jeff.v.swanson@gmail.com>
2021-12-11 21:30:18 -0700

    WIP problem 53

---
+++ 0053_MaximumSubarray/python/solution.py
@@ -0,0 +1,39 @@
+"""
+53. Maximum Subarray
+Given an integer array nums, find the contiguous subarray (containing at least one
+number) which has the largest sum and return its sum.
+
+A subarray is a contiguous part of an array.
+
+Examples
+--------
+Example 1:
+    Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+    Output: 6
+    Explanation: [4,-1,2,1] has the largest sum = 6.
+
+Example 2:
+    Input: nums = [1]
+    Output: 1
+
+Example 3:
+    Input: nums = [5,4,-1,7,8]
+    Output: 23
+
+Constraints
+-----------
+* 1 <= nums.length <= 105
+* -104 <= nums[i] <= 104
+"""
+
+def initial_pass(nums: list[int]) -> int:
+    sum = 0
+    if len(nums) < 1:
+        return sum
+
+    for num in nums:
+        sum += num
+    
+    sum = max(sum, initial_pass(nums[1:]))
+
+    return sum
---
+++ 0053_MaximumSubarray/python/test_solution.py
@@ -0,0 +1,16 @@
+import pytest
+
+import solution
+
+
+@pytest.mark.parametrize(
+    "nums,expected",
+    [
+        ([-2, 1, -3, 4, -1, 2, 1, -5, 4], 6),
+        ([1], 1),
+        ([5, 4, -1, 7, 8], 23),
+    ]
+)
+def test_initial_pass(nums, expected):
+    got = solution.initial_pass(nums)
+    assert got == expected
